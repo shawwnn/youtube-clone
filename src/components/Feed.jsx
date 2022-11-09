a8077bc29c510b4b9382169b7ea1f68ac41f5089
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-09 14:32:40 +0800

    Feed and Sidebar I

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Box, Stack, Typography } from "@mui/material"
+import Sidebar from "./Sidebar"
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack
+      sx={{ flexDirection : { sx: "column", md: "row"} }}
+    >
+      <Box
+        sx={{ 
+          height: { sx: 'auto', md: '92vh'},
+          borderRight: '1px solid #3D3D3D', px: { sx: 0, md: 2} 
+        }}
+      >
+        <Sidebar />
+        <Typography 
+          className="copyright" 
+          variant="body2"
+          sx={{
+            mt: 1.5, color: '#FFF'
+          }}
+        >
+          Copyright 2022 Shawn Youtube 
+        </Typography>
+      </Box>
+
+    </Stack>
   )
 }
 
