e65c66e979f827748c5535972c4a49a9e3fb9772
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-12 13:16:52 +0800

    Channel card CSS style correction

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -8,7 +8,13 @@ const ChannelCard = ({ channelDetail }) => (
   <Box
     sx={{
       boxShadow: 'none',
-      borderRadius: '20px'
+      borderRadius: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: { xs: '356px', md: '320px'},
+      height: '326px',
+      margin: 'auto'
     }}
   >
     <Link to={`/channel/${channelDetail?.id?.channelId}`} >
