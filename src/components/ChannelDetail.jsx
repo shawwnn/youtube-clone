424f137642dc5211019e7aabdba99f783e4074f3
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-15 08:30:53 +0800

    Channel Page I: Get Channel Details (useParams/State/Effect)

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { Box } from "@mui/material"
+
+import { Videos, ChannelCard } from "./"
+import { fetchFromAPI } from "../utils/fetchFromAPI"
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  console.log(channelDetail);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then(data => setChannelDetail(data?.items[0]));
+  }, [id]);
+
   return (
-    <div>ChannelDetail</div>
+    <div>{id}</div>
   )
 }
 
