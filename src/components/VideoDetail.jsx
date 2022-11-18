61cefa7aa099cef6a326643a9a91316dac076516
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-18 22:53:51 +0800

    Video III: Show Title from the api into useState

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -24,6 +24,9 @@ const VideoDetail = () => {
         <Box flex={1}>
           <Box sx={{ width: "100%", position: 'sticky', top: '86px' }}>
             <ReactPlayer className="react-player" url={`https://www.youtube.com/watch?v=${id}`} controls />
+            <Typography color="#FFF" variant="h5" fontWeight="bold" p={2}>
+              {videoDetails.snippet.title}
+            </Typography>
           </Box>
         </Box>
       </Stack>
