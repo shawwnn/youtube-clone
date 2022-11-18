ac42355dde0d1e4697e5d9196a41e6d262fc433d
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-18 13:45:57 +0800

    Video I: Testing Layouting and partial show of ReactPlayer

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Link, useParams } from "react-router-dom"
+
+import ReactPlayer from "react-player"
+
+import { Typography, Box, Stack } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+
+import { Videos } from './';
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const VideoDetail = () => {
   return (
-    <div>VideoDetail</div>
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: 'column', md: 'row'}}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: 'sticky', top: '86px' }}>
+            <ReactPlayer />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
