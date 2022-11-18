2f389c4f11cd4c2832c648389699663d66f45ba2
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-18 13:00:05 +0800

    Search I: Copied Entire Feed Component and just removed the sidebar

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { Box, Typography } from "@mui/material"
+import { Videos } from "./"
+import { fetchFromAPI } from "../utils/fetchFromAPI"
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=`)
+    .then((data) => setVideos(data.items))
+  }, []);
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overFlowY: 'auto', height: '90vh', flex: 2}}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        Search Results for: <span style={{ color: '#F31503' }}>videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
   )
 }
 
